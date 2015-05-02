df8f942e15c1be0aba8fef7f22e4cffc3d5c6bcf
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-05-02 16:12:58 +0900

    Added query patterns "himawari" "sakurako".

--- src/main/kotlin/jp/gecko655/bot/akalin/AkalinBot.kt
+++ src/main/kotlin/jp/gecko655/bot/akalin/AkalinBot.kt
@@ -8,11 +8,14 @@ public class AkalinBot : AbstractCron() {
     override fun twitterCron() {
         //Twitterに書き出し
         val status = StatusUpdate(" ")
-        when ((Math.random() * 10).toInt()) {
+        when ((Math.random() * 13).toInt()) {
             0 -> updateStatusWithMedia(status, "赤座あかり ゆるゆり", 100)
             in 1..3 -> updateStatusWithMedia(status, "歳納京子 ゆるゆり", 100)
             in 4..6 -> updateStatusWithMedia(status, "船見結衣 ゆるゆり", 100)
             in 7..9 -> updateStatusWithMedia(status, "吉川ちなつ ゆるゆり", 100)
+            10 -> updateStatusWithMedia(status, "向日葵 ゆるゆり", 100)
+            11 -> updateStatusWithMedia(status, "櫻子 ゆるゆり", 100)
+            else -> updateStatusWithMedia(status, "ゆるゆり", 100)
         }
 
     }
--- src/main/kotlin/jp/gecko655/bot/akalin/AkalinReply.kt
+++ src/main/kotlin/jp/gecko655/bot/akalin/AkalinReply.kt
@@ -52,11 +52,14 @@ public class AkalinReply : AbstractCron() {
                     //auto reply
                     val update = StatusUpdate("@" + reply.getUser().getScreenName() + " ")
                     update.setInReplyToStatusId(reply.getId())
-                    when ((Math.random() * 10).toInt()) {
+                    when ((Math.random() * 13).toInt()) {
                         0 -> updateStatusWithMedia(update, "赤座あかり ゆるゆり かわいい", 100)
                         in 1..3 -> updateStatusWithMedia(update, "歳納京子 ゆるゆり かわいい", 100)
                         in 4..6 -> updateStatusWithMedia(update, "船見結衣 ゆるゆり かわいい", 100)
                         in 7..9 -> updateStatusWithMedia(update, "吉川ちなつ ゆるゆり かわいい", 100)
+                        10 -> updateStatusWithMedia(update, "向日葵 ゆるゆり かわいい", 100)
+                        11 -> updateStatusWithMedia(update, "櫻子 ゆるゆり かわいい", 100)
+                        else -> updateStatusWithMedia(update, "ゆるゆり かわいい", 100)
                     }
                 }
             }
