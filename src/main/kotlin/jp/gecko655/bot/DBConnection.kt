8b7e37f1e3d2782db7877882633c566470d800d7
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-06-14 21:16:07 +0900

    Revised codes for test.

--- src/main/kotlin/jp/gecko655/bot/DBConnection.kt
+++ src/main/kotlin/jp/gecko655/bot/DBConnection.kt
@@ -69,6 +69,10 @@ object DBConnection {
     }
 
     public fun isInBlackList(link: String): Boolean {
+        if(blackListCollectionName==null){
+            logger.log(Level.WARNING, "No collection to mongodb")
+            return false
+        }
         val blackListCollection = db.getCollection(blackListCollectionName)
         return blackListCollection.find(Filters.eq<String>(urlKey, link)).iterator().hasNext()
     }
