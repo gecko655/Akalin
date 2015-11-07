97971f07a417c2670dbb7d230eddc80e71fd426f
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-11-08 01:24:05 +0900

    Let it use docker test in travis

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-docker run -d --name akalin  --env-file secretenv gecko655/akalin
+SECRETENV_PATH="secretenv_release"
+if [ $# -gt 0 ] && [ $1 = test ]; then
+  SECRETENV_PATH="secretenv_test"
+fi
+docker run -d --name akalin  --env-file $SECRETENV_PATH gecko655/akalin
